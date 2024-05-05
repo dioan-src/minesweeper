199b3cffca40267e4d0562dd63ca6e4f15bda1ec
dioan-src <dimeioannou@outlook.com>
2024-05-05 15:12:20 +0300

    [3] new-fix -add function to find adjacent mines for each squares -split board initializer func to simpler funcs -use adjacentmines func on bored initializer -create board of Square class -fix display func to use Square functions

--- Board.php
+++ Board.php
@@ -1,4 +1,5 @@
 <?php
+require_once __DIR__ . '/Square.php';
 
 // class for  the minsweeper board
 class Board
@@ -7,6 +8,11 @@ class Board
     private int $height;
     private int $minesNum;    
     private array $board;
+    const SUB_BOARD_HEIGHT = 3;
+    const SUB_BOARD_LENGTH = 3;
+    const IS_HIDDEN = 0;
+    const SHOW_MINE = 'X ';
+    const SHOW_HIDDEN_SQUARE = '\u{25A0} ';
 
     public function __construct(int $length, int $height, int $minesNum)
     {
@@ -58,12 +64,73 @@ class Board
 
     public function initializeBoard()
     {
+        $boardWithMines = $this->createBoardWithRandomizedMines();
+
+        //TODO CHANGE THIS - I DONT WANT TO SET A BOARD AND THEN RESET IT.
+        $this->setBoard($boardWithMines);
+
+        $boardMadeOfSquares = $this->initializeSquaresOfBoard($boardWithMines);
+         
+        $this->setBoard($boardMadeOfSquares);
+    }
+
+    public function createBoardWithRandomizedMines(): array
+    {
+        //create mines array with $this->minesNum elements, based on the constructor of Board
         $minesArray = array_fill(0, ($this->minesNum), 1);
+        //create non-mines array with x elements, based on the constructor of Board
         $nonMinesArray = array_fill(0, ($this->height*$this->length-$this->minesNum), 0);
+        //merge arrays and shuffle to get the final array
         $finalArray = array_merge($minesArray, $nonMinesArray);
         shuffle($finalArray);
-        $boardValues = array_chunk($finalArray, $this->height);
-        $this->setBoard($boardValues);
+        //chunk the array based on the constructor height, to create the final board
+        return array_chunk($finalArray, $this->height);;
+    }
+
+    public function initializeSquaresOfBoard(array $boardValues): array
+    {
+        $boardOfSquares = [];
+        for ($i = 0; $i < $this->height; $i++) {
+
+            for ($j = 0; $j < $this->length; $j++) {
+                $boardOfSquares[$i][$j] = new Square( $this->getAdjacentMines($i, $j), $j, $i, (bool)$boardValues[$i][$j], false, false);
+            }
+            
+        }
+
+        return $boardOfSquares;
+    }
+
+    public function getAdjacentMines(int $centerHeight, int $centerLength): int
+    {
+        $adjacentMines = 0;
+        // echo 'center is ' . $centerHeight . ' ' . $centerLength . ' ';echo PHP_EOL;
+        // echo 'limits are ' . $this->height . ' ' . $this->length . ' ';echo PHP_EOL;
+
+        $startHeight = $centerHeight - 1;                   //set start height of outer loop
+        $startLength = $centerLength - 1;                   //set start length of outer loop
+        $endHeight = $startHeight + self::SUB_BOARD_HEIGHT; //set finishing height of inner loop
+        $endLength = $startLength + self::SUB_BOARD_LENGTH; //set finishing length of inner loop
+        
+        for ($h = $startHeight ; $h < $endHeight; $h++) {
+            for ($l = $startLength ; $l < $endLength; $l++) {
+                // echo $h . ' ' . $l . ' ';
+                //exclude center cell
+                if ($h == $centerHeight && $l == $centerLength){ continue;}
+                // echo ' after 1';
+                //exclude cell out of array
+                if ($h < 0 || $h >= $this->height){ continue;}
+                // echo ' after 2';
+                if ($l < 0 || $l >= $this->length){ continue;} 
+                // echo ' after 3';    
+                // echo ' added ' . $this->board[$h][$l] . ' ';
+                $adjacentMines += $this->board[$h][$l];
+                // echo PHP_EOL;
+            }
+        }
+        // echo ' got ' . $adjacentMines;
+        // die();
+        return $adjacentMines;
     }
 
     public function display()
@@ -76,6 +143,7 @@ class Board
             echo $j + 1 . " ";
         }
         echo PHP_EOL;
+        echo '   _ _ _ _ _ _ _ _' . PHP_EOL;
 
         // Print board with row numbers and cells
         for ($i = 0; $i < $this->height; $i++) {
@@ -84,12 +152,18 @@ class Board
 
             // Print cells
             for ($j = 0; $j < $this->length; $j++) {
-                echo ($board[$i][$j] == 1) ? 'X ' : '0 ' ;
-                // echo "\u{25A0} ";
-                //TODO Use square for hidden values, X for mine and value or ' ' for the rest
+                if ($board[$i][$j]->getHasMine()){
+                    echo self::SHOW_MINE;
+                }else{
+                    if ($board[$i][$j]->getValue()){
+                        echo $board[$i][$j]->getValue() . ' ';
+                    }else{
+                        echo '  ';
+                    }
+                }
+                // echo ($board[$i][$j]->getHasMine()) ? 'X ' : $board[$i][$j]->getValue() . ' ' ;
             }
             echo PHP_EOL;
         }
     }
-
 }
--- Run.php
+++ Run.php
@@ -3,12 +3,5 @@ require_once __DIR__ . '/Square.php';
 require_once __DIR__ . '/Board.php';
 
 $board = new Board(8, 8, 15);
-// echo 'after all';
-// echo PHP_EOL;
-// echo $board->getHeight();
-// echo PHP_EOL;
-// echo $board->getLength();
-// echo PHP_EOL;
-// echo $board->getMinesNum();
-// echo PHP_EOL;
+//TODO fix infinite loop on different height / length values
 $board->display();
